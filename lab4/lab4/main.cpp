605f2680deea49cd0207fb570edf847b955d313e
Oleksii Kyrylchuk <olety7@gmail.com>
2015-03-19 13:55:43 +0100

    added lab4

---
+++ lab4/lab4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Oleksii Kyrylchuk on 3/19/15.
+//  Copyright (c) 2015 olety. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
